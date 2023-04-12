bf707416b5cb707f4c04ac34b988bc0f89f4ba97
inlogicstudio <119988830+inlogicstudio@users.noreply.github.com>
2023-04-12 14:47:22 +0200

    Initial commit - v1.0

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+//Offside.js minimal setup
+var myOffside = offside( '#nav', {
+    slidingElementsSelector:'#content',
+    buttonsSelector: '#toggle, .another-button',
+    slidingSide: 'right',
+});
